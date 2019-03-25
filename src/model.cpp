e745fdb3f1495eeaa8a230c3dbfa7bd15e5a667f
Omegastick <omegastick@hotmail.co.uk>
2019-03-25 05:50:46 +0000

    Get libtorch running

--- src/model.cpp
+++ src/model.cpp
@@ -1,10 +1,14 @@
-#include <torch/csrc/api/include/torch/torch.h>
+#include <iostream>
 
-#include "CppRl/model.h"
+#include <torch/torch.h>
 
-namespace CppRl
-{
+#include "cpprl/model.h"
+
 int main(int /*argc*/, char * /*argv*/ [])
 {
-}
+    torch::Tensor x = torch::rand({100, 100}, c10::TensorOptions());
+    torch::Tensor y = torch::rand({100, 100});
+    std::cout << x * y << std::endl;
+
+    return 0;
 }
