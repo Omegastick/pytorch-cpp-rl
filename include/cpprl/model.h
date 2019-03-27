de75f0df14bed28bf063cb650d73ddbdabd599dd
Omegastick <omegastick@hotmail.co.uk>
2019-03-27 07:29:35 +0000

    Implement MlpBase and CnnBase

--- include/cpprl/model.h
+++ include/cpprl/model.h
@@ -24,9 +24,9 @@ class NNBase : public nn::Module
            unsigned int recurrent_input_size,
            unsigned int hidden_size);
 
-    std::vector<torch::Tensor> forward_gru(torch::Tensor &x,
-                                           torch::Tensor &hxs,
-                                           torch::Tensor &masks);
+    std::vector<torch::Tensor> forward_gru(torch::Tensor x,
+                                           torch::Tensor hxs,
+                                           torch::Tensor masks);
     unsigned int get_hidden_size() const;
 
     inline bool is_recurrent() const { return recurrent; }
@@ -38,25 +38,25 @@ class PolicyImpl : public nn::Module
     std::shared_ptr<NNBase> base;
     // std::unique_ptr<Distribution> dist;
 
-    std::vector<torch::Tensor> forward_gru(torch::Tensor &x,
-                                           torch::Tensor &hxs,
-                                           torch::Tensor &masks);
+    std::vector<torch::Tensor> forward_gru(torch::Tensor x,
+                                           torch::Tensor hxs,
+                                           torch::Tensor masks);
 
   public:
     PolicyImpl(c10::IntArrayRef observation_shape,
                ActionSpace action_space,
                std::shared_ptr<NNBase> base);
 
-    std::vector<torch::Tensor> act(torch::Tensor &inputs,
-                                   torch::Tensor &rnn_hxs,
-                                   torch::Tensor &masks) const;
-    std::vector<torch::Tensor> evaluate_actions(torch::Tensor &inputs,
-                                                torch::Tensor &rnn_hxs,
-                                                torch::Tensor &masks,
-                                                torch::Tensor &actions) const;
-    torch::Tensor get_values(torch::Tensor &inputs,
-                             torch::Tensor &rnn_hxs,
-                             torch::Tensor &masks) const;
+    std::vector<torch::Tensor> act(torch::Tensor inputs,
+                                   torch::Tensor rnn_hxs,
+                                   torch::Tensor masks);
+    std::vector<torch::Tensor> evaluate_actions(torch::Tensor inputs,
+                                                torch::Tensor rnn_hxs,
+                                                torch::Tensor masks,
+                                                torch::Tensor actions);
+    torch::Tensor get_values(torch::Tensor inputs,
+                             torch::Tensor rnn_hxs,
+                             torch::Tensor masks);
 
     inline bool is_recurrent() const { return base->is_recurrent(); }
     inline unsigned int get_hidden_size() const
@@ -69,17 +69,17 @@ TORCH_MODULE(Policy);
 class CnnBase : public NNBase
 {
   private:
-    std::unique_ptr<nn::Module> main;
-    std::unique_ptr<nn::Module> critic_linear;
+    nn::Sequential main;
+    nn::Sequential critic_linear;
 
   public:
     CnnBase(unsigned int num_inputs,
             bool recurrent = false,
             unsigned int hidden_size = 512);
 
-    std::vector<torch::Tensor> forward(torch::Tensor &inputs,
-                                       torch::Tensor &hxs,
-                                       torch::Tensor &masks) const;
+    std::vector<torch::Tensor> forward(torch::Tensor inputs,
+                                       torch::Tensor hxs,
+                                       torch::Tensor masks);
 };
 
 class MlpBase : public NNBase
@@ -94,8 +94,16 @@ class MlpBase : public NNBase
             bool recurrent = false,
             unsigned int hidden_size = 64);
 
-    std::vector<torch::Tensor> forward(torch::Tensor &inputs,
-                                       torch::Tensor &hxs,
-                                       torch::Tensor &masks) const;
+    std::vector<torch::Tensor> forward(torch::Tensor inputs,
+                                       torch::Tensor hxs,
+                                       torch::Tensor masks);
 };
+
+struct FlattenImpl : nn::Module
+{
+    torch::Tensor forward(torch::Tensor x);
+};
+TORCH_MODULE(Flatten);
+
+void init_weights(torch::OrderedDict<std::string, torch::Tensor> parameters, double gain);
 }
--- src/model.cpp
+++ src/model.cpp
@@ -50,21 +50,21 @@ unsigned int NNBase::get_hidden_size() const
     return 1;
 }
 
-std::vector<torch::Tensor> NNBase::forward_gru(torch::Tensor &x,
-                                               torch::Tensor &hxs,
-                                               torch::Tensor &masks)
+std::vector<torch::Tensor> NNBase::forward_gru(torch::Tensor x,
+                                               torch::Tensor rnn_hxs,
+                                               torch::Tensor masks)
 {
-    if (x.size(0) == hxs.size(0))
+    if (x.size(0) == rnn_hxs.size(0))
     {
         auto gru_output = gru->forward(x.unsqueeze(0),
-                                       (hxs * masks).unsqueeze(0));
+                                       (rnn_hxs * masks).unsqueeze(0));
         return {gru_output.output.squeeze(0), gru_output.state.squeeze(0)};
     }
     else
     {
         // x is a (timesteps, agents, -1) tensor that has been flattened to
         // (timesteps * agents, -1)
-        auto agents = hxs.size(0);
+        auto agents = rnn_hxs.size(0);
         auto timesteps = x.size(0) / agents;
 
         // Unflatten
@@ -92,7 +92,7 @@ std::vector<torch::Tensor> NNBase::forward_gru(torch::Tensor &x,
         has_zeros_vec.insert(has_zeros_vec.begin(), {0});
         has_zeros_vec.push_back(timesteps);
 
-        hxs = hxs.unsqueeze(0);
+        rnn_hxs = rnn_hxs.unsqueeze(0);
         std::vector<torch::Tensor> outputs;
         for (unsigned int i = 0; i < has_zeros_vec.size() - 1; ++i)
         {
@@ -106,16 +106,16 @@ std::vector<torch::Tensor> NNBase::forward_gru(torch::Tensor &x,
                 x.index({torch::arange(start_idx,
                                        end_idx,
                                        TensorOptions(ScalarType::Long))}),
-                hxs * masks[start_idx].view({1, -1, 1}));
+                rnn_hxs * masks[start_idx].view({1, -1, 1}));
 
             outputs.push_back(gru_output.output);
         }
 
         // x is a (timesteps, agents, -1) tensor
         x = torch::cat(outputs, 1).squeeze(0);
-        hxs = hxs.squeeze(0);
+        rnn_hxs = rnn_hxs.squeeze(0);
 
-        return {x, hxs};
+        return {x, rnn_hxs};
     }
 }
 
@@ -123,24 +123,24 @@ PolicyImpl::PolicyImpl(c10::IntArrayRef /*observation_shape*/,
                        ActionSpace /*action_space*/,
                        std::shared_ptr<NNBase> base) : base(base) {}
 
-std::vector<torch::Tensor> PolicyImpl::act(torch::Tensor & /*inputs*/,
-                                           torch::Tensor & /*rnn_hxs*/,
-                                           torch::Tensor & /*masks*/) const
+std::vector<torch::Tensor> PolicyImpl::act(torch::Tensor /*inputs*/,
+                                           torch::Tensor /*rnn_hxs*/,
+                                           torch::Tensor /*masks*/)
 {
     return std::vector<torch::Tensor>();
 }
 
-std::vector<torch::Tensor> PolicyImpl::evaluate_actions(torch::Tensor & /*inputs*/,
-                                                        torch::Tensor & /*rnn_hxs*/,
-                                                        torch::Tensor & /*masks*/,
-                                                        torch::Tensor & /*actions*/) const
+std::vector<torch::Tensor> PolicyImpl::evaluate_actions(torch::Tensor /*inputs*/,
+                                                        torch::Tensor /*rnn_hxs*/,
+                                                        torch::Tensor /*masks*/,
+                                                        torch::Tensor /*actions*/)
 {
     return std::vector<torch::Tensor>();
 }
 
-torch::Tensor PolicyImpl::get_values(torch::Tensor & /*inputs*/,
-                                     torch::Tensor & /*rnn_hxs*/,
-                                     torch::Tensor & /*masks*/) const
+torch::Tensor PolicyImpl::get_values(torch::Tensor /*inputs*/,
+                                     torch::Tensor /*rnn_hxs*/,
+                                     torch::Tensor /*masks*/)
 {
     return torch::Tensor();
 }
@@ -148,13 +148,46 @@ torch::Tensor PolicyImpl::get_values(torch::Tensor & /*inputs*/,
 CnnBase::CnnBase(unsigned int num_inputs,
                  bool recurrent,
                  unsigned int hidden_size)
-    : NNBase(recurrent, num_inputs, hidden_size) {}
-
-std::vector<torch::Tensor> CnnBase::forward(torch::Tensor & /*inputs*/,
-                                            torch::Tensor & /*hxs*/,
-                                            torch::Tensor & /*masks*/) const
+    : NNBase(recurrent, hidden_size, hidden_size),
+      main(register_module(
+          "main", nn::Sequential(
+                      nn::Conv2d(nn::Conv2dOptions(num_inputs, 32, 8).stride(4)),
+                      nn::Functional(torch::relu),
+                      nn::Conv2d(nn::Conv2dOptions(32, 64, 4).stride(2)),
+                      nn::Functional(torch::relu),
+                      nn::Conv2d(nn::Conv2dOptions(64, 32, 3).stride(1)),
+                      nn::Functional(torch::relu),
+                      Flatten(),
+                      nn::Linear(32 * 7 * 7, hidden_size),
+                      nn::Functional(torch::relu)))),
+      critic_linear(register_module("critic_linear", nn::Linear(hidden_size, 1)))
 {
-    return std::vector<torch::Tensor>();
+    // Why this is commented out: https://github.com/pytorch/pytorch/issues/18518
+    // init_weights(main->named_parameters(), sqrt(2.));
+    init_weights(critic_linear->named_parameters(), 1);
+
+    train();
+}
+
+std::vector<torch::Tensor> CnnBase::forward(torch::Tensor inputs,
+                                            torch::Tensor rnn_hxs,
+                                            torch::Tensor masks)
+{
+    auto x = main->forward(inputs / 255.);
+
+    if (is_recurrent())
+    {
+        auto gru_output = forward_gru(x, rnn_hxs, masks);
+        x = gru_output[0];
+        rnn_hxs = gru_output[1];
+    }
+
+    return {critic_linear->forward(x), x, rnn_hxs};
+}
+
+torch::Tensor FlattenImpl::forward(torch::Tensor x)
+{
+    return x.view({x.size(0), -1});
 }
 
 MlpBase::MlpBase(unsigned int num_inputs,
@@ -175,18 +208,27 @@ MlpBase::MlpBase(unsigned int num_inputs,
                         nn::Functional(torch::tanh)))),
       critic_linear(register_module("critic_linear", nn::Linear(hidden_size, 1)))
 {
-    init_weights(actor->named_parameters(), sqrt(2));
-    init_weights(critic->named_parameters(), sqrt(2));
-    init_weights(critic_linear->named_parameters(), sqrt(2));
+    init_weights(actor->named_parameters(), sqrt(2.));
+    init_weights(critic->named_parameters(), sqrt(2.));
+    init_weights(critic_linear->named_parameters(), sqrt(2.));
 
     train();
 }
 
-std::vector<torch::Tensor> MlpBase::forward(torch::Tensor & /*inputs*/,
-                                            torch::Tensor & /*hxs*/,
-                                            torch::Tensor & /*masks*/) const
+std::vector<torch::Tensor> MlpBase::forward(torch::Tensor inputs,
+                                            torch::Tensor rnn_hxs,
+                                            torch::Tensor masks)
 {
-    return std::vector<torch::Tensor>();
+    auto x = inputs;
+
+    if (is_recurrent())
+    {
+        auto gru_output = forward_gru(x, rnn_hxs, masks);
+        x = gru_output[0];
+        rnn_hxs = gru_output[1];
+    }
+
+    return {critic_linear->forward(x), x, rnn_hxs};
 }
 
 TEST_CASE("init_weights()")
@@ -218,9 +260,9 @@ TEST_CASE("NNBase")
             " agent")
     {
         auto inputs = torch::rand({4, 5});
-        auto hxs = torch::rand({4, 10});
+        auto rnn_hxs = torch::rand({4, 10});
         auto masks = torch::zeros({4, 1});
-        auto outputs = base->forward_gru(inputs, hxs, masks);
+        auto outputs = base->forward_gru(inputs, rnn_hxs, masks);
 
         REQUIRE(outputs.size() == 2);
 
@@ -228,7 +270,7 @@ TEST_CASE("NNBase")
         CHECK(outputs[0].size(0) == 4);
         CHECK(outputs[0].size(1) == 10);
 
-        // hxs
+        // rnn_hxs
         CHECK(outputs[1].size(0) == 4);
         CHECK(outputs[1].size(1) == 10);
     }
@@ -237,9 +279,9 @@ TEST_CASE("NNBase")
             "multiple agents")
     {
         auto inputs = torch::rand({12, 5});
-        auto hxs = torch::rand({4, 10});
+        auto rnn_hxs = torch::rand({4, 10});
         auto masks = torch::zeros({12, 1});
-        auto outputs = base->forward_gru(inputs, hxs, masks);
+        auto outputs = base->forward_gru(inputs, rnn_hxs, masks);
 
         REQUIRE(outputs.size() == 2);
 
@@ -247,7 +289,7 @@ TEST_CASE("NNBase")
         CHECK(outputs[0].size(0) == 12);
         CHECK(outputs[0].size(1) == 10);
 
-        // hxs
+        // rnn_hxs
         CHECK(outputs[1].size(0) == 4);
         CHECK(outputs[1].size(1) == 10);
     }
@@ -266,9 +308,9 @@ TEST_CASE("MlpBase")
     SUBCASE("Output tensors are correct shapes")
     {
         auto inputs = torch::rand({4, 5});
-        auto hxs = torch::rand({4, 10});
+        auto rnn_hxs = torch::rand({4, 10});
         auto masks = torch::zeros({4, 1});
-        auto outputs = base.forward(inputs, hxs, masks);
+        auto outputs = base.forward(inputs, rnn_hxs, masks);
 
         REQUIRE(outputs.size() == 3);
 
@@ -299,9 +341,9 @@ TEST_CASE("CnnBase")
     SUBCASE("Output tensors are correct shapes")
     {
         auto inputs = torch::rand({4, 3, 84, 84});
-        auto hxs = torch::rand({4, 10});
+        auto rnn_hxs = torch::rand({4, 10});
         auto masks = torch::zeros({4, 1});
-        auto outputs = base->forward(inputs, hxs, masks);
+        auto outputs = base->forward(inputs, rnn_hxs, masks);
 
         REQUIRE(outputs.size() == 3);
 
@@ -333,9 +375,9 @@ TEST_CASE("Policy")
     SUBCASE("act() output tensors are correct shapes")
     {
         auto inputs = torch::rand({4, 3});
-        auto hxs = torch::rand({4, 10});
+        auto rnn_hxs = torch::rand({4, 10});
         auto masks = torch::zeros({4, 1});
-        auto outputs = policy->act(inputs, hxs, masks);
+        auto outputs = policy->act(inputs, rnn_hxs, masks);
 
         REQUIRE(outputs.size() == 3);
 
@@ -359,10 +401,10 @@ TEST_CASE("Policy")
     SUBCASE("evaluate_actions() output tensors are correct shapes")
     {
         auto inputs = torch::rand({4, 3});
-        auto hxs = torch::rand({4, 10});
+        auto rnn_hxs = torch::rand({4, 10});
         auto masks = torch::zeros({4, 1});
         auto actions = torch::rand({4, 5});
-        auto outputs = policy->evaluate_actions(inputs, hxs, masks, actions);
+        auto outputs = policy->evaluate_actions(inputs, rnn_hxs, masks, actions);
 
         REQUIRE(outputs.size() == 3);
 
@@ -385,13 +427,45 @@ TEST_CASE("Policy")
     SUBCASE("get_values() output tensor is correct shapes")
     {
         auto inputs = torch::rand({4, 3});
-        auto hxs = torch::rand({4, 10});
+        auto rnn_hxs = torch::rand({4, 10});
         auto masks = torch::zeros({4, 1});
-        auto outputs = policy->get_values(inputs, hxs, masks);
+        auto outputs = policy->get_values(inputs, rnn_hxs, masks);
 
         // Value
         CHECK(outputs.size(0) == 4);
         CHECK(outputs.size(1) == 1);
+    }
+}
+
+TEST_CASE("Flatten")
+{
+    auto flatten = Flatten();
+
+    SUBCASE("Flatten converts 3 dimensional vector to 2 dimensional")
+    {
+        auto input = torch::rand({5, 5, 5});
+        auto output = flatten->forward(input);
+
+        CHECK(output.size(0) == 5);
+        CHECK(output.size(1) == 25);
+    }
+
+    SUBCASE("Flatten converts 5 dimensional vector to 2 dimensional")
+    {
+        auto input = torch::rand({2, 2, 2, 2, 2});
+        auto output = flatten->forward(input);
+
+        CHECK(output.size(0) == 2);
+        CHECK(output.size(1) == 16);
+    }
+
+    SUBCASE("Flatten converts 1 dimensional vector to 2 dimensional")
+    {
+        auto input = torch::rand({10});
+        auto output = flatten->forward(input);
+
+        CHECK(output.size(0) == 10);
+        CHECK(output.size(1) == 1);
     }
 }
 }
