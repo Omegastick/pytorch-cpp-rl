74a9b9168e4371774f2465d53b3c355eccb602e5
Omegastick <omegastick@hotmail.co.uk>
2019-03-28 03:04:23 +0000

    Fix Categorical::sample() with regards to multidimensional probabilities

--- include/cpprl/distributions/categorical.h
+++ include/cpprl/distributions/categorical.h
@@ -19,7 +19,7 @@ class Categorical
   public:
     Categorical(const torch::Tensor *probs, const torch::Tensor *logits);
 
-    torch::Tensor sample(c10::IntArrayRef sample_shape);
+    torch::Tensor sample(torch::IntArrayRef sample_shape);
     torch::Tensor log_prob(torch::Tensor value);
     torch::Tensor entropy();
 };
--- src/distributions/categorical.cpp
+++ src/distributions/categorical.cpp
@@ -34,7 +34,8 @@ Categorical::Categorical(const torch::Tensor *probs,
     num_events = param.size(-1);
     if (param.dim() > 1)
     {
-        batch_shape = param.sizes().slice(param.dim() - 1).vec();
+        batch_shape = param.sizes().vec();
+        batch_shape.resize(batch_shape.size() - 1);
     }
 }
 
@@ -63,11 +64,12 @@ torch::Tensor Categorical::log_prob(torch::Tensor /*value*/)
     return torch::Tensor();
 }
 
-torch::Tensor Categorical::sample(c10::IntArrayRef sample_shape)
+torch::Tensor Categorical::sample(torch::IntArrayRef sample_shape)
 {
     auto ext_sample_shape = extended_shape(sample_shape);
-    ext_sample_shape.insert(ext_sample_shape.end(), {num_events});
-    auto exp_probs = probs.expand(ext_sample_shape);
+    auto param_shape = ext_sample_shape;
+    param_shape.insert(param_shape.end(), {num_events});
+    auto exp_probs = probs.expand(param_shape);
     torch::Tensor probs_2d;
     if (probs.dim() == 1 or probs.size(0) == 1)
     {
@@ -78,7 +80,7 @@ torch::Tensor Categorical::sample(c10::IntArrayRef sample_shape)
         probs_2d = exp_probs.contiguous().view({-1, num_events});
     }
     auto sample_2d = torch::multinomial(probs_2d, 1, true);
-    return sample_2d.contiguous().view(sample_shape);
+    return sample_2d.contiguous().view(ext_sample_shape);
 }
 
 TEST_CASE("Categorical")
@@ -100,6 +102,43 @@ TEST_CASE("Categorical")
         auto less_than_0 = output < 0;
         CHECK(!more_than_4.any().item().toBool());
         CHECK(!less_than_0.any().item().toBool());
+    }
+
+    SUBCASE("Generated tensors are of the right shape")
+    {
+        float probabilities[] = {0.2, 0.2, 0.2, 0.2, 0.2};
+        auto probabilities_tensor = torch::from_blob(probabilities, {5});
+        auto dist = Categorical(&probabilities_tensor, nullptr);
+
+        CHECK(dist.sample({20}).sizes().vec() == std::vector<long>{20});
+        CHECK(dist.sample({2, 20}).sizes().vec() == std::vector<long>{2, 20});
+        CHECK(dist.sample({1, 2, 3, 4, 5}).sizes().vec() == std::vector<long>{1, 2, 3, 4, 5});
+    }
+
+    SUBCASE("Multi-dimensional input probabilities are handled correctly")
+    {
+        SUBCASE("Generated tensors are of the right shape")
+        {
+            float probabilities[2][4] = {{0.5, 0.5, 0.0, 0.0}, {0.25, 0.25, 0.25, 0.25}};
+            auto probabilities_tensor = torch::from_blob(probabilities, {2, 4});
+            auto dist = Categorical(&probabilities_tensor, nullptr);
+
+            CHECK(dist.sample({20}).sizes().vec() == std::vector<long>{20, 2});
+            CHECK(dist.sample({10, 5}).sizes().vec() == std::vector<long>{10, 5, 2});
+        }
+
+        SUBCASE("Generated tensors have correct probabilities")
+        {
+            float probabilities[2][4] = {{0, 1, 0, 0}, {0, 0, 0, 1}};
+            auto probabilities_tensor = torch::from_blob(probabilities, {2, 4});
+            auto dist = Categorical(&probabilities_tensor, nullptr);
+
+            auto output = dist.sample({20});
+            auto sum = output.sum({0});
+
+            CHECK(sum[0].item().toInt() == 20);
+            CHECK(sum[1].item().toInt() == 60);
+        }
     }
 }
 }
