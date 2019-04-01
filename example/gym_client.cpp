51911d0c82ec2cb805df494e63ddd4823b2d9940
Omegastick <omegastick@hotmail.co.uk>
2019-04-01 09:50:44 +0000

    Run A2C algorithm in gym_client.cpp

--- example/gym_client.cpp
+++ example/gym_client.cpp
@@ -1,9 +1,30 @@
 #include <spdlog/spdlog.h>
 
+#include <cpprl/spaces.h>
+#include <cpprl/storage.h>
+#include <cpprl/algorithms/a2c.h>
+#include <cpprl/model/mlp_base.h>
+#include <cpprl/model/policy.h>
+
 #include "communicator.h"
 #include "requests.h"
 
 using namespace gym_client;
+using namespace cpprl;
+
+template <typename T>
+std::vector<T> flatten_2d_vector(std::vector<std::vector<T>> const &input)
+{
+    std::vector<T> output;
+
+    for (auto const &sub_vector : input)
+    {
+        output.reserve(output.size() + sub_vector.size());
+        output.insert(output.end(), sub_vector.cbegin(), sub_vector.cend());
+    }
+
+    return output;
+}
 
 int main(int argc, char *argv[])
 {
@@ -26,15 +47,69 @@ int main(int argc, char *argv[])
     auto reset_param = std::make_shared<ResetParam>();
     Request<ResetParam> reset_request("reset", reset_param);
     communicator.send_request(reset_request);
-    auto observation = communicator.get_response<ResetResponse>()->observation;
+    auto observation_vec = flatten_2d_vector<float>(communicator.get_response<ResetResponse>()->observation);
+    auto observation = torch::from_blob(observation_vec.data(), {4, 4});
+
+    auto base = std::make_shared<MlpBase>(4, false, 5);
+    ActionSpace space{"Discrete", {2}};
+    Policy policy(space, base);
+    RolloutStorage storage(100, 4, {4}, space, 5);
+    A2C a2c(policy, 0.5, 1e-3, 0.0001);
 
     for (int i = 0; i < 1000; ++i)
     {
-        auto step_param = std::make_shared<StepParam>();
-        step_param->actions = {{1}, {0}, {1}, {0}};
-        step_param->render = true;
-        Request<StepParam> step_request("step", step_param);
-        communicator.send_request(step_request);
-        communicator.get_response<StepResponse>();
+
+        for (int step = 0; step < 100; ++step)
+        {
+            std::vector<torch::Tensor> act_result;
+            {
+                torch::NoGradGuard no_grad;
+                act_result = policy->act(observation,
+                                         torch::Tensor(),
+                                         torch::ones({2, 1}));
+            }
+            long *actions_array = act_result[1].data<long>();
+            std::vector<std::vector<int>> actions;
+            actions.resize(4);
+            for (int i = 0; i < 4; ++i)
+            {
+                actions[i] = {static_cast<int>(actions_array[i])};
+            }
+
+            auto step_param = std::make_shared<StepParam>();
+            step_param->actions = actions;
+            step_param->render = false;
+            Request<StepParam> step_request("step", step_param);
+            communicator.send_request(step_request);
+            auto step_result = communicator.get_response<StepResponse>();
+            auto rewards = flatten_2d_vector<float>(step_result->reward);
+            auto dones = torch::zeros({4, 1});
+            for (int i = 0; i < 4; ++i)
+            {
+                dones[i][0] = 1 - static_cast<int>(step_result->done[i][0]);
+            }
+
+            storage.insert(observation,
+                           torch::zeros({4, 5}),
+                           act_result[1],
+                           act_result[2],
+                           act_result[0],
+                           torch::from_blob(rewards.data(), {4, 1}),
+                           dones);
+        }
+
+        torch::Tensor next_value;
+        {
+            torch::NoGradGuard no_grad;
+            next_value = policy->get_values(
+                                   storage.get_observations()[-1],
+                                   storage.get_hidden_states()[-1],
+                                   storage.get_masks()[-1])
+                             .detach();
+        }
+        storage.compute_returns(next_value, false, 0., 0.9);
+
+        a2c.update(storage);
+        storage.after_update();
     }
 }
--- src/algorithms/a2c.cpp
+++ src/algorithms/a2c.cpp
@@ -71,21 +71,21 @@ std::vector<UpdateDatum> A2C::update(RolloutStorage &rollouts)
 
 TEST_CASE("A2C")
 {
-    auto base = std::make_shared<MlpBase>(1, false, 10);
+    auto base = std::make_shared<MlpBase>(1, false, 5);
     ActionSpace space{"Discrete", {2}};
     Policy policy(space, base);
-    RolloutStorage storage(5, 2, {1}, space, 10);
+    RolloutStorage storage(5, 2, {1}, space, 5);
     A2C a2c(policy, 0.5, 1e-9, 0.001);
 
-    SUBCASE("update() learns basic game")
+    SUBCASE("update() learns basic pattern")
     {
-        // The game is: If the action matches the input, give a reward of 1, otherwise -1
+        // The reward is the action
         auto pre_game_probs = policy->get_probs(
             torch::ones({2, 1}),
-            torch::zeros({2, 10}),
+            torch::zeros({2, 5}),
             torch::ones({2, 1}));
 
-        for (int i = 0; i < 1000; ++i)
+        for (int i = 0; i < 10; ++i)
         {
             for (int j = 0; j < 5; ++j)
             {
@@ -100,21 +100,9 @@ TEST_CASE("A2C")
                 }
                 auto actions = act_result[1];
 
-                float rewards_array[2];
-                for (int process = 0; process < actions.size(0); ++process)
-                {
-                    if ((actions[process] == observation[process].to(torch::kLong)).item().toBool())
-                    {
-                        rewards_array[process] = 1;
-                    }
-                    else
-                    {
-                        rewards_array[process] = -1;
-                    }
-                }
-                auto rewards = torch::from_blob(rewards_array, {2, 1});
+                auto rewards = actions;
                 storage.insert(observation,
-                               torch::zeros({2, 10}),
+                               torch::zeros({2, 5}),
                                actions,
                                act_result[2],
                                act_result[0],
@@ -139,7 +127,70 @@ TEST_CASE("A2C")
 
         auto post_game_probs = policy->get_probs(
             torch::ones({2, 1}),
-            torch::zeros({2, 10}),
+            torch::zeros({2, 5}),
+            torch::ones({2, 1}));
+
+        INFO("Pre-training probabilities: \n"
+             << pre_game_probs << "\n");
+        INFO("Post-training probabilities: \n"
+             << post_game_probs << "\n");
+        CHECK(post_game_probs[0][0].item().toDouble() <
+              pre_game_probs[0][0].item().toDouble());
+        CHECK(post_game_probs[0][1].item().toDouble() >
+              pre_game_probs[0][1].item().toDouble());
+    }
+
+    SUBCASE("update() learns basic game")
+    {
+        // The game is: If the action matches the input, give a reward of 1, otherwise -1
+        auto pre_game_probs = policy->get_probs(
+            torch::ones({2, 1}),
+            torch::zeros({2, 5}),
+            torch::ones({2, 1}));
+
+        for (int i = 0; i < 1000; ++i)
+        {
+            for (int j = 0; j < 5; ++j)
+            {
+                auto observation = torch::randint(0, 2, {2, 1});
+
+                std::vector<torch::Tensor> act_result;
+                {
+                    torch::NoGradGuard no_grad;
+                    act_result = policy->act(observation,
+                                             torch::Tensor(),
+                                             torch::ones({2, 1}));
+                }
+                auto actions = act_result[1];
+
+                auto rewards = ((actions == observation.to(torch::kLong)).to(torch::kFloat) * 2) - 1;
+                storage.insert(observation,
+                               torch::zeros({2, 5}),
+                               actions,
+                               act_result[2],
+                               act_result[0],
+                               rewards,
+                               torch::ones({2, 1}));
+            }
+
+            torch::Tensor next_value;
+            {
+                torch::NoGradGuard no_grad;
+                next_value = policy->get_values(
+                                       storage.get_observations()[-1],
+                                       storage.get_hidden_states()[-1],
+                                       storage.get_masks()[-1])
+                                 .detach();
+            }
+            storage.compute_returns(next_value, false, 0., 0.9);
+
+            a2c.update(storage);
+            storage.after_update();
+        }
+
+        auto post_game_probs = policy->get_probs(
+            torch::ones({2, 1}),
+            torch::zeros({2, 5}),
             torch::ones({2, 1}));
 
         INFO("Pre-training probabilities: \n"
